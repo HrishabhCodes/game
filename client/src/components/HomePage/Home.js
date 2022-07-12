1cc2ec338c4096cb10488fc701fc065d38e824d4
Hrishabh Jain <hrishabh507@gmail.com>
2022-07-13 00:54:54 +0530

    fix: Avatar bug

--- client/src/components/HomePage/Home.js
+++ client/src/components/HomePage/Home.js
@@ -3,76 +3,62 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import GameChat from "../GameChat/GameChat";
 import SocketContext from "../../context/socketContext";
-import { storage } from "../../firebase";
-import { ref, getDownloadURL } from "firebase/storage";
 import Logo from "../../assets/logo_doodle.png";
-import Loading from "../Loading/Loading";
 import { motion } from "framer-motion";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
 const Home = () => {
   const [showGameChat, setShowGameChat] = useState(false);
-  const [URL, setURL] = useState("");
+  const avatar = localStorage.getItem("avatar") || 1;
   const ctx = useContext(SocketContext);
   ctx.setStart(false);
 
-  const avatarRef = ref(storage, `avatars/${1}.png`);
-  getDownloadURL(avatarRef)
-    .then((url) => setURL(url))
-    .catch((err) => console.log(err));
-
   return (
     <>
-      {URL === "" ? (
-        <div>
-          <Loading />
+      <motion.div
+        className="home-page-cont"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <div className="logo">
+          <LazyLoadImage
+            effect="blur"
+            className="vert-move"
+            src={Logo}
+            alt=""
+          />
         </div>
-      ) : (
-        <motion.div
-          className="home-page-cont"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        >
-          <div className="logo">
-            <LazyLoadImage
-              effect="blur"
-              className="vert-move"
-              src={Logo}
-              alt=""
-            />
-          </div>
-          {!showGameChat ? (
-            <div className="user-cont">
-              <Link to="/profile" className="user-info">
-                <div className="user-img-cont">
-                  <LazyLoadImage
-                    effect="blur"
-                    className="user-img"
-                    src={URL}
-                    alt=""
-                  />
-                </div>
-                <div className="username">{ctx.name}</div>
-              </Link>
-              <div className="quick-play-cont">
-                <Link to="/play" className="game-info">
-                  <div className="play-icon">
-                    <i className="fa-solid fa-play"></i>
-                  </div>
-
-                  <div className="mode-any link-light">
-                    Game Mode: <strong className="any-word">ANY</strong>
-                  </div>
-                </Link>
+        {!showGameChat ? (
+          <div className="user-cont">
+            <Link to="/profile" className="user-info">
+              <div className="user-img-cont">
+                <LazyLoadImage
+                  effect="blur"
+                  className="user-img"
+                  src={`avatars/${avatar}.png`}
+                  alt="avatar"
+                />
               </div>
+              <div className="username">{ctx.name}</div>
+            </Link>
+            <div className="quick-play-cont">
+              <Link to="/play" className="game-info">
+                <div className="play-icon">
+                  <i className="fa-solid fa-play"></i>
+                </div>
+
+                <div className="mode-any link-light">
+                  Game Mode: <strong className="any-word">ANY</strong>
+                </div>
+              </Link>
             </div>
-          ) : (
-            <GameChat showGC={setShowGameChat} modal={setShowGameChat} />
-          )}
-        </motion.div>
-      )}
+          </div>
+        ) : (
+          <GameChat showGC={setShowGameChat} modal={setShowGameChat} />
+        )}
+      </motion.div>
     </>
   );
 };
--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,51 +1,22 @@
-import React, { useState, useRef, useContext } from "react";
-import { storage } from "../../firebase";
-import { ref, getDownloadURL } from "firebase/storage";
+import React, { useState, useContext } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import "./Profile.css";
 import SocketContext from "../../context/socketContext";
 import { Box, Modal } from "@mui/material";
-import Avatar1 from "../../assets/avatars/1.png";
-import Avatar2 from "../../assets/avatars/2.png";
-import Avatar3 from "../../assets/avatars/3.png";
-import Avatar4 from "../../assets/avatars/4.png";
-import Avatar5 from "../../assets/avatars/5.png";
-import Avatar6 from "../../assets/avatars/6.png";
-import Avatar7 from "../../assets/avatars/7.png";
-import Avatar8 from "../../assets/avatars/8.png";
-import Avatar9 from "../../assets/avatars/9.png";
-import Avatar10 from "../../assets/avatars/10.png";
-import Avatar11 from "../../assets/avatars/11.png";
-import Avatar12 from "../../assets/avatars/12.png";
-import Avatar13 from "../../assets/avatars/13.png";
-import Avatar14 from "../../assets/avatars/14.png";
-import Avatar15 from "../../assets/avatars/15.png";
-import Avatar16 from "../../assets/avatars/16.png";
-import Avatar17 from "../../assets/avatars/17.png";
-import Avatar18 from "../../assets/avatars/18.png";
-import Avatar19 from "../../assets/avatars/19.png";
-import Avatar20 from "../../assets/avatars/20.png";
-import Avatar21 from "../../assets/avatars/21.png";
-import Avatar22 from "../../assets/avatars/22.png";
-import Avatar23 from "../../assets/avatars/23.png";
-import Avatar24 from "../../assets/avatars/24.png";
 import { motion } from "framer-motion";
+const arr = [...Array(24).keys()];
 
 const Profile = () => {
   const ctx = useContext(SocketContext);
   const [open, setOpen] = useState(false);
-  const [selAvatar, setSelAvatar] = useState("");
-  const [url, setUrl] = useState();
+  const [selAvatar, setSelAvatar] = useState(
+    localStorage.getItem("avatar") || 1
+  );
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const [disable, setDisable] = useState(true);
   const [change, setChange] = useState(false);
-  const avatarRef = ref(storage, `avatars/${ctx.avatar}.png`);
-
-  getDownloadURL(avatarRef).then((URL) => {
-    setUrl(URL);
-  });
 
   const style = {
     position: "absolute",
@@ -85,7 +56,7 @@ const Profile = () => {
       <div className="avatar-cont">
         <LazyLoadImage
           effect="blur"
-          src={url}
+          src={`avatars/${selAvatar}.png`}
           className="avatar-img"
           alt="Avatar"
         />
@@ -103,306 +74,28 @@ const Profile = () => {
               <LazyLoadImage
                 effect="blur"
                 className="display"
-                src={
-                  selAvatar === 2
-                    ? Avatar2
-                    : selAvatar === 3
-                    ? Avatar3
-                    : selAvatar === 4
-                    ? Avatar4
-                    : selAvatar === 5
-                    ? Avatar5
-                    : selAvatar === 6
-                    ? Avatar6
-                    : selAvatar === 7
-                    ? Avatar7
-                    : selAvatar === 8
-                    ? Avatar8
-                    : selAvatar === 9
-                    ? Avatar9
-                    : selAvatar === 10
-                    ? Avatar10
-                    : selAvatar === 11
-                    ? Avatar11
-                    : selAvatar === 12
-                    ? Avatar12
-                    : selAvatar === 13
-                    ? Avatar13
-                    : selAvatar === 14
-                    ? Avatar14
-                    : selAvatar === 15
-                    ? Avatar15
-                    : selAvatar === 16
-                    ? Avatar16
-                    : selAvatar === 17
-                    ? Avatar17
-                    : selAvatar === 18
-                    ? Avatar18
-                    : selAvatar === 19
-                    ? Avatar19
-                    : selAvatar === 20
-                    ? Avatar20
-                    : selAvatar === 21
-                    ? Avatar21
-                    : selAvatar === 22
-                    ? Avatar22
-                    : selAvatar === 23
-                    ? Avatar23
-                    : selAvatar === 24
-                    ? Avatar24
-                    : Avatar1
-                }
+                src={`avatars/${selAvatar}.png`}
                 onClick={() => handleAvatar()}
-                alt=""
+                alt="avatar"
               />
             </div>
             <div className="img-options">
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar1}
-                onClick={() => handleAvatar(1)}
-                alt=""
-                className={
-                  selAvatar === 1 ? `selected avatar1 avatar` : `avatar avatar1`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar2}
-                onClick={() => handleAvatar(2)}
-                alt=""
-                className={
-                  selAvatar === 2 ? `selected avatar2 avatar` : `avatar avatar2`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar3}
-                onClick={() => handleAvatar(3)}
-                alt=""
-                className={
-                  selAvatar === 3 ? `selected avatar3 avatar` : `avatar avatar3`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar4}
-                onClick={() => handleAvatar(4)}
-                alt=""
-                className={
-                  selAvatar === 4 ? `selected avatar6 avatar` : `avatar avatar6`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar5}
-                onClick={() => handleAvatar(5)}
-                alt=""
-                className={
-                  selAvatar === 5 ? `selected avatar6 avatar` : `avatar avatar6`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar6}
-                onClick={() => handleAvatar(6)}
-                alt=""
-                className={
-                  selAvatar === 6 ? `selected avatar6 avatar` : `avatar avatar6`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar7}
-                onClick={() => handleAvatar(7)}
-                alt=""
-                className={
-                  selAvatar === 7 ? `selected avatar7 avatar` : `avatar avatar7`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar8}
-                onClick={() => handleAvatar(8)}
-                alt=""
-                className={
-                  selAvatar === 8 ? `selected avatar8 avatar` : `avatar avatar8`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar9}
-                onClick={() => handleAvatar(9)}
-                alt=""
-                className={
-                  selAvatar === 9 ? `selected avatar9 avatar` : `avatar avatar9`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar10}
-                onClick={() => handleAvatar(10)}
-                alt=""
-                className={
-                  selAvatar === 10
-                    ? `selected avatar10 avatar`
-                    : `avatar avatar10`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar11}
-                onClick={() => handleAvatar(11)}
-                alt=""
-                className={
-                  selAvatar === 11
-                    ? `selected avatar11 avatar`
-                    : `avatar avatar11`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar12}
-                onClick={() => handleAvatar(12)}
-                alt=""
-                className={
-                  selAvatar === 12
-                    ? `selected avatar12 avatar`
-                    : `avatar avatar12`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar13}
-                onClick={() => handleAvatar(13)}
-                alt=""
-                className={
-                  selAvatar === 13
-                    ? `selected avatar13 avatar`
-                    : `avatar avatar13`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar14}
-                onClick={() => handleAvatar(14)}
-                alt=""
-                className={
-                  selAvatar === 14
-                    ? `selected avatar14 avatar`
-                    : `avatar avatar14`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar15}
-                onClick={() => handleAvatar(15)}
-                alt=""
-                className={
-                  selAvatar === 15
-                    ? `selected avatar15 avatar`
-                    : `avatar avatar15`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar16}
-                onClick={() => handleAvatar(16)}
-                alt=""
-                className={
-                  selAvatar === 16
-                    ? `selected avatar16 avatar`
-                    : `avatar avatar16`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar17}
-                onClick={() => handleAvatar(17)}
-                alt=""
-                className={
-                  selAvatar === 17
-                    ? `selected avatar17 avatar`
-                    : `avatar avatar17`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar18}
-                onClick={() => handleAvatar(18)}
-                alt=""
-                className={
-                  selAvatar === 18
-                    ? `selected avatar18 avatar`
-                    : `avatar avatar18`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar19}
-                onClick={() => handleAvatar(19)}
-                alt=""
-                className={
-                  selAvatar === 19
-                    ? `selected avatar19 avatar`
-                    : `avatar avatar19`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar20}
-                onClick={() => handleAvatar(20)}
-                alt=""
-                className={
-                  selAvatar === 20
-                    ? `selected avatar20 avatar`
-                    : `avatar avatar20`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar21}
-                onClick={() => handleAvatar(21)}
-                alt=""
-                className={
-                  selAvatar === 21
-                    ? `selected avatar21 avatar`
-                    : `avatar avatar21`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar22}
-                onClick={() => handleAvatar(22)}
-                alt=""
-                className={
-                  selAvatar === 22
-                    ? `selected avatar22 avatar`
-                    : `avatar avatar22`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar23}
-                onClick={() => handleAvatar(23)}
-                alt=""
-                className={
-                  selAvatar === 23
-                    ? `selected avatar23 avatar`
-                    : `avatar avatar23`
-                }
-              />
-              <LazyLoadImage
-                effect="blur"
-                src={Avatar24}
-                onClick={() => handleAvatar(24)}
-                alt=""
-                className={
-                  selAvatar === 24
-                    ? `selected avatar24 avatar`
-                    : `avatar avatar24`
-                }
-              />
+              {arr.map((avatar, i) => {
+                return (
+                  <LazyLoadImage
+                    key={i}
+                    effect="blur"
+                    src={`avatars/${i + 1}.png`}
+                    onClick={() => handleAvatar(i + 1)}
+                    alt="avatar"
+                    className={
+                      selAvatar === i + 1
+                        ? `selected avatar${i + 1} avatar`
+                        : `avatar avatar${i + 1}`
+                    }
+                  />
+                );
+              })}
             </div>
           </Box>
         </Modal>
